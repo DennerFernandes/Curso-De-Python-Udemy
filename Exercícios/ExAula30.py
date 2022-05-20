5fa42dcda14659fe85ab4088a946d7b30cb7d010
Denner Fernandes <dennerc06@gmail.com>
2022-05-20 18:10:16 -0300

    Exercícios-da-aula-30-prontos

--- Exercícios/ExAula30.py
+++ Exercícios/ExAula30.py
@@ -1,9 +1,16 @@
+############ Exercício 1 ############ 
+
+print('-'*30)
+print('Exercício 1'.center(30))
+print('-'*30)
+
 numero1 =  input('Digite um número: ')
 numero2 =  input('Digite um outro número: ')
 
 
 if numero1.isdigit() and numero2.isdigit():
-
+    numero1 = int(numero1)
+    numero2 = int(numero2)
     if numero1 % 2 == 0:
         print(f'O número {numero1} é par')
     else:
@@ -17,11 +24,37 @@ if numero1.isdigit() and numero2.isdigit():
 else:
     print('Por favor, repita o programa e digite números inteiros válidos')
 
+############  Ex 2 ############ 
+
+print('-'*30)
+print('Exercício 2'.center(30))
+print('-'*30)
+
+horario = input('Qual o horário atual? ')
+
+if horario.isdigit():
+    horario = int(horario)
+    if horario <= 11:
+        print('Bom dia!')
+    elif 17 >= horario >= 12:
+        print('Boa tarde!')
+    else:
+        print('Boa noite!')
+    
+else:
+    print('Horário inválido.')
+
 ############ Ex 2 ############ 
 
-print()
-horario = input('Qual o horário atual? ')
-if horario.isnumeric():
-    horario = int(horario)
-    if horario > 11:
-        print('Bom dia!')
+print('-'*30)
+print('Exercício 3'.center(30))
+print('-'*30)
+
+nome = input('Digite seu primeiro nome: ')
+
+if len(nome) <= 4:
+    print('Seu nome é curto')
+elif len(nome) <= 6:
+    print('Seu nome é normal')
+elif len(nome) > 6:
+    print('Seu nome é muito grande')
